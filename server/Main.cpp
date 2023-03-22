6ae8090b91cb7ba12c4050348581115f208c8360
fanpengfei1 <fanpengfei@jd.com>
2023-03-22 08:42:14 +0000

    commit Main

--- server/Main.cpp
+++ server/Main.cpp
@@ -1,8 +1,8 @@
 #include <getopt.h>
 #include <unistd.h>
 
-#include "server.h"
 #include "lib/util.h"
+#include "server.h"
 #include "spdlog/spdlog.h"
 
 int main(int argc, char **argv) {
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
                                          //  longopts的最后一个元素必须是全0填充，否则会报段错误
                                          {0, 0, 0, 0}};
 
+  // 读取stdargs参数
   u_int32_t ctl_port = 0;
   u_int32_t proxy_port = 0;
   int work_thread_nums = 0;
@@ -40,6 +41,7 @@ int main(int argc, char **argv) {
   work_thread_nums = work_thread_nums > 0 ? work_thread_nums : 1;
   printf("ctl_port:%d; proxy_port:%d; work_thread_nums: %d\n", ctl_port, proxy_port,
          work_thread_nums);
+
   Server server(work_thread_nums, ctl_port, proxy_port);
   server.start();
 }
