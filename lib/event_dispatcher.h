56f040b0716dbe27c91e4c1e9edee74dfe84bab2
fanpengfei <fanpengfei1@jd.com>
2023-04-11 15:35:35 +0800

    fw

--- lib/event_dispatcher.h
+++ lib/event_dispatcher.h
@@ -4,13 +4,13 @@
 #include "channel.h"
 
 class EventDispatcher {
-public:
-    EventDispatcher(){};
-    virtual ~EventDispatcher(){};
-    virtual void PollAdd(SP_Channel) = 0;
-    virtual void PollMod(SP_Channel) = 0;
-    virtual void PollDel(SP_Channel) = 0;
-    virtual std::vector<SP_Channel> WaitForReadyChannels() = 0;
+ public:
+  EventDispatcher(){};
+  virtual ~EventDispatcher(){};  /// 析构函数是virtual的，动态绑定，派生类的内存释放
+  virtual void PollAdd(SP_Channel) = 0;  /// 纯虚函数： 派生类需实现接口
+  virtual void PollMod(SP_Channel) = 0;
+  virtual void PollDel(SP_Channel) = 0;
+  virtual std::vector<SP_Channel> WaitForReadyChannels() = 0;
 };
 
-using SP_EventDispatcher = std::shared_ptr<EventDispatcher>;
+using SP_EventDispatcher = std::shared_ptr<EventDispatcher>;  // 别名： using xxx  = yyy
